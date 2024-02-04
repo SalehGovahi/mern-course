41c3a58e38db3b3aff9183c20ae133ea0b698bcd
Saleh Govahi <115073548+SalehGovahi@users.noreply.github.com>
2024-02-04 17:03:04 +0330

    Add components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList/GoalList';
+import NewGoal from './components/GoalList/NewGoal/NewGoal';
 
 class App extends Component {
     state = {
-        age: 42,
+        courseGoals : [
+            {id : 'cg1', text: 'Finish the course'},
+            {id : 'cg2', text: 'Learn all about Course Main Topic'},
+            {id : 'cg3', text: 'Help other students in the Course Q&A'},
+        ],
     };
     
     render() {
         return (
             <div className='course-goals'>
                 <h2>Course Goals</h2>
-                <h1>{this.state.age}</h1>
-                <GoalList />
+                <NewGoal />
+                <GoalList goals={this.state.courseGoals}/>
             </div>
         );
     }
