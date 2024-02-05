3494f0d68517b4efd6894350d5c8cc1e29bedd7e
Saleh Govahi <115073548+SalehGovahi@users.noreply.github.com>
2024-02-05 15:54:14 +0330

    Add first app with router and multi component (React)

---
+++ building-frontend/src/user/components/UsersList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = props => {
+    if (props.items.length === 0){
+        return (
+            <div>
+                <h2>No user found.</h2>
+            </div>
+        );
+    }
+    return (
+        <ul className="users-list">
+            {props.items.map(user => (
+                <UserItem
+                    key={user.id}
+                    id={user.id}
+                    image={user.image}
+                    name={user.name}
+                    placeCount={user.place}  
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default UsersList;
+
---
+++ building-frontend/src/user/pages/Users.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'user1',
+            image:
+                'https://picsum.photos/200',
+            places: 3
+        }
+    ];
+
+    
+    return <UsersList items={USERS} />;
+};
+
+export default Users;
