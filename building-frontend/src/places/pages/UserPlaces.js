1c3806f534a09a7bf00d822d202627636a6fda69
Saleh Govahi <115073548+SalehGovahi@users.noreply.github.com>
2024-02-07 13:56:14 +0330

    Add UserPlaces

---
+++ building-frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+import './UserPlaces.css';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State',
+        description: 'One of the most famous building',
+        imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipMDv3C-fXXvEvttpkkgNG5Rg52BwR4SBaA1w0dR=s680-w680-h510',
+        address: '20 W 34th St., New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State',
+        description: 'One of the most famous building',
+        imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipMDv3C-fXXvEvttpkkgNG5Rg52BwR4SBaA1w0dR=s680-w680-h510',
+        address: '20 W 34th St., New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
+        },
+        creator: 'u2'
+    }
+];
+
+const UserPlaces = props => {
+    return <PlaceList items={DUMMY_PLACES}/>;
+};
+
+export default UserPlaces;
--- building-frontend/src/user/pages/Users.js
+++ building-frontend/src/user/pages/Users.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import UsersList from '../components/UsersList';
+import UsersList from "../components/UsersList";
 
 const Users = () => {
   const USERS = [
     {
-      id: 'u1',
-      name: 'Max Schwarz',
-      image:
-      'https://picsum.photos/200',
-      places: 3
-    }
+      id: "u1",
+      name: "Max Schwarz",
+      image: "https://picsum.photos/200",
+      places: 3,
+    },
   ];
 
   return <UsersList items={USERS} />;
