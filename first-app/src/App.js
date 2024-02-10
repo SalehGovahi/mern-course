e0ee368eb7ebb484d90778c21d2a7521525572a4
Saleh Govahi <115073548+SalehGovahi@users.noreply.github.com>
2024-02-10 09:29:22 +0330

    Add Modal to show popup

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import GoalList from './components/GoalList/GoalList';
-import NewGoal from './components/NewGoal/NewGoal';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 const App = () => {
-  const [courseGoals, setCourseGoals] = useState([
-    { id: 'cg1', text: 'Finish the course' },
-    { id: 'cg2', text: 'Learn all about Course Main Topic' },
-    { id: 'cg3', text: 'Help other students in the Course Q&A' },
-  ]);
-
-  const addNewGoalController = (newGoal) => {
-    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
-  };
-
   return (
-    <div className='course-goals'>
-      <h2>Course Goals</h2>
-      <NewGoal onAddGoal={addNewGoalController} />
-      <GoalList goals={courseGoals} />
-    </div>
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
